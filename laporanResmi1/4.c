db7362227d58f2d0de895d3f6b9bf9336856a07f
willyyypatootieee <blueplayz26@gmail.com>
2025-08-29 09:40:49 +0700

    add some assignment

---
+++ laporanResmi1/4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    int jumlah;
+    float rata_rata;
+    int kuadrat_a, kuadrat_b;
+
+    printf("Masukkan bilangan pertama: ");
+    scanf("%d", &a);
+    printf("Masukkan bilangan kedua: ");
+    scanf("%d", &b);
+
+    jumlah = a + b;
+    rata_rata = (a + b) / 2.0;
+    kuadrat_a = a * a;
+    kuadrat_b = b * b;
+
+    printf("Jumlah: %d\n", jumlah);
+    printf("Rata-rata: %.2f\n", rata_rata);
+    printf("Kuadrat bilangan pertama: %d\n", kuadrat_a);
+    printf("Kuadrat bilangan kedua: %d\n", kuadrat_b);
+
+    return 0;
+}
