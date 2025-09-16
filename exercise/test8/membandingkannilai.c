221307b5d170070af5122bcdd5ff6cb23994f9a8
willyyypatootieee <blueplayz26@gmail.com>
2025-09-16 09:05:49 +0700

    Add initial implementation of variabletotal.c

---
+++ exercise/test8/membandingkannilai.c
@@ -0,0 +1,30 @@
+// #include <stdio.h>
+
+// void main() {
+//     int bil1;
+
+//     printf("masukkan angka: \n");
+//     scanf("%d", &bil1);
+
+//     if ( bil1 >= 65 ) {
+//         printf("Anda Lulus!\n");
+//     } else {
+//         printf("Maaf anda tidak lulus.");
+//     }
+// }
+
+#include <stdio.h>
+
+void main() {
+    int sum;
+
+    printf("masukkan angka: \n");
+    scanf("%d", &sum);
+
+    if ( sum >= 65 ) {
+        printf("Anda Lulus!\n");
+
+    } else {
+        printf("Maaf anda tidak lulus.");
+    }
+}
